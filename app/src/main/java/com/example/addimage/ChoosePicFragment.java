dd608a3c99f8cea351f871975582e56687c66331
sireen moed <moadsireen@gmail.com>
2025-10-08 13:05:14 +0300

    image commit

---
+++ app/src/main/java/com/example/addimage/ChoosePicFragment.java
@@ -0,0 +1,92 @@
+package com.example.addimage;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Bundle;
+
+import androidx.annotation.RequiresApi;
+import androidx.fragment.app.Fragment;
+
+import android.provider.MediaStore;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link ChoosePicFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class ChoosePicFragment extends Fragment {
+
+    private static final int GALLERY_REQUEST_CODE = 123;
+    private ImageView ivProfile;
+
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+    public ChoosePicFragment() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment ChoosePicFragment.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static ChoosePicFragment newInstance(String param1, String param2) {
+        ChoosePicFragment fragment = new ChoosePicFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_choose_pic, container, false);
+    }
+
+    public void ChoosePic(View view){
+        Intent galleryIntent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        startActivityForResult(galleryIntent, GALLERY_REQUEST_CODE);
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.UPSIDE_DOWN_CAKE)
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == GALLERY_REQUEST_CODE && resultCode == getActivity().RESULT_OK && data != null) {
+            Uri selectedImageUri = data.getData();
+            ivProfile.setImageURI(selectedImageUri);
+        }
+
+
+}
+}
---
+++ app/src/main/java/com/example/addimage/imageView.java
@@ -0,0 +1,10 @@
+package com.example.addimage;
+
+import android.net.Uri;
+
+public class imageView {
+    public void setImageURI(Uri selectedImageUri) {
+    }
+
+
+}
